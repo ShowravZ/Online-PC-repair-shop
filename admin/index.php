6c3ac06640c21cf2a60429a760be0321a8dcf793
ShowravZ <md.showrav.zaman@g.bracu.ac.bd>
2021-12-30 02:59:18 +0600

    No

---
+++ admin/index.php
@@ -0,0 +1,33 @@
+<?php include('partials/menu.php'); ?>
+        
+        <!-- Section Starts Here -->
+        <div class="main-content">
+            <div class="wrapper">
+                <h1 class="text-center">DASHBOARD</h1>
+
+                <div class="boxes">
+                    <h1>5</h1>
+                    Categories
+                </div>
+
+                <div class="boxes">
+                    <h1>5</h1>
+                    Categories
+                </div>
+
+                <div class="boxes">
+                    <h1>5</h1>
+                    Categories
+                </div>
+
+                <div class="boxes">
+                    <h1>5</h1>
+                    Categories
+                </div>
+                <div class="clearfix"></div>
+
+            </div>
+        </div>
+        <!-- Section End Here -->
+        
+ <?php include('partials/end.php'); ?>      
